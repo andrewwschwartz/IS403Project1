f6a3c0badf65ce4962bf162f0ae4c8927f93fdb1
Josue Romo <josueromo@Josues-MBP.app.byu.edu>
2017-11-11 15:22:12 -0700

    added pics and updated mission info

--- IS403Project1/IS403Project1/Controllers/MissionController.cs
+++ IS403Project1/IS403Project1/Controllers/MissionController.cs
@@ -19,33 +19,33 @@ namespace IS403Project1.Controllers
             if (iMishName == "Brazil")
             {
                 ViewBag.MishName = iMishName;
-                ViewBag.PresName = "President Dude";
+                ViewBag.PresName = "President Tanner";
                 ViewBag.MishAddress = "123 Futbol Sao Paolo, Brazil";
                 ViewBag.Language = "Portuguese";
-                ViewBag.Climate = "Hot and steamy";
-                ViewBag.Religion = "Catholics";
+                ViewBag.Climate = "High humidity, varied weather according to season. No snow and stays above freezing";
+                ViewBag.Religion = "Catholicism and Evangelical";
                 ViewBag.Picture = "Brazil.png";
             }
 
             else if (iMishName == "New York")
             {
                 ViewBag.MishName = iMishName;
-                ViewBag.PresName = "President Dude";
+                ViewBag.PresName = "President Calderwood";
                 ViewBag.MishAddress = "123 Manhattan New York, New York";
                 ViewBag.Language = "English/Spanish";
-                ViewBag.Climate = "Hot and cold";
-                ViewBag.Religion = "Jehovah's Witness";
+                ViewBag.Climate = "High Humidity, very hot in the summer and very cold in the winter. Usually snows during winter months";
+                ViewBag.Religion = "Catholicism";
                 ViewBag.Picture = "New York.jpg";
             }
 
             else if (iMishName == "Arizona")
             {
                 ViewBag.MishName = iMishName;
-                ViewBag.PresName = "President Dude";
-                ViewBag.MishAddress = "123 Futbol Sao Paolo, Arizona";
+                ViewBag.PresName = "President Jenkins";
+                ViewBag.MishAddress = "123 Stapley Dr Mesa, Arizona";
                 ViewBag.Language = "English";
-                ViewBag.Climate = "Hot and steamy";
-                ViewBag.Religion = "Catholics";
+                ViewBag.Climate = "Super Hot and very dry";
+                ViewBag.Religion = "Mormon";
                 ViewBag.Picture = "Arizona.jpg";
             }
 
