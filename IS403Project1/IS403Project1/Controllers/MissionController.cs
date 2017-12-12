79b9dcb78ceeccb75f505b33a4a34aeb2750fced
Tanner Morse <Tannermorse33@gmail.com>
2017-12-11 20:34:36 -0700

    made mission questions also pull from database via viewbag

--- IS403Project1/IS403Project1/Controllers/MissionController.cs
+++ IS403Project1/IS403Project1/Controllers/MissionController.cs
@@ -20,22 +20,22 @@ namespace IS403Project1.Controllers
         public ActionResult MissionPage(int mission_ID)
         {
             
-            /*
-            IEnumerable<MissionAndQuestions> missions = db.Database.SqlQuery<MissionAndQuestions>(
-                "SELECT Missions.mission_ID, Missions.missionName, Missions.missionPresidentName, Missions.missionAddress, Missions.missionLanguage, " +
-                "Missions.missionClimate, Missions.missionDominantReligion, Missions.missionFlag, " + 
-                "MissionQuestions.MissionQuestion_ID, MissionQuestions.question, MissionQuestions.answer, " +
-                "MissionQuestions.user_ID " +
-                "FROM Missions inner join MissionQuestions on Missions.mission_ID = MissionQuestions.mission_ID "+
-                "WHERE Missions.mission_ID = " + mission_ID);
-                */
+            
+            IEnumerable<MissionQuestions> questions = db.Database.SqlQuery<MissionQuestions>(
+                "SELECT MissionQuestions.MissionQuestion_ID, MissionQuestions.question, MissionQuestions.answer, " +
+                "MissionQuestions.user_ID, MissionQuestions.mission_ID " +
+                "FROM MissionQuestions "+
+                "WHERE MissionQuestions.mission_ID = " + mission_ID);
+                
 
             IEnumerable<Missions> missions = db.Database.SqlQuery<Missions>(
                 "SELECT Missions.mission_ID, Missions.missionName, Missions.missionPresidentName, Missions.missionAddress, Missions.missionLanguage, " +
                 "Missions.missionClimate, Missions.missionDominantReligion, Missions.missionFlag " +
                 "FROM Missions " +
                 "WHERE Missions.mission_ID = " + mission_ID);
-               
+
+            ViewBag.questions = questions;
+            ViewBag.missions = missions;   
 
             return View(missions);
         }
